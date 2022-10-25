31d3dfc70c822e3875e3741b7f4726ffac6dc7e3
zlwindystreet <116582857+zlwindystreet@users.noreply.github.com>
2022-10-25 21:49:43 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+# 公众号配置
+# 公众号appId
+app_id = "wx58d0a0e01484c9b2"
+# 公众号appSecret
+app_secret = "599c768137a47b54725b24ec254bb51c"
+# 模板消息id
+template_id = "coZdtQiTMNBM-ein7WfTZg1BWpmrcmrfSnfRMafx8_8"
+
+# 这是openid
+user = [""oVEqa5mXsU-iUzSNjZZJJoxJPtxM""]
+
+# 信息配置
+# 所在省份
+province = "北京"
+# 所在城市
+city = "北京"
+# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+birthday = "1998-8-1"
+# 在一起的日子，格式同上
+love_date = "2001-1-1"
+
